814733cf5679fce33a1bcd63fe79fd7044e6629f
Jefferson Chen <f1jefferson@yahoo.com.tw>
2023-05-30 15:23:01 +0800

    add basic query in house repo

--- rental_web/src/main/java/com/group18/rental_web/repository/HouseRepo.java
+++ rental_web/src/main/java/com/group18/rental_web/repository/HouseRepo.java
@@ -9,4 +9,12 @@ import java.util.List;
 @Repository
 public interface HouseRepo extends JpaRepository<House, Integer> {
     public List<House> findAll();
+
+    // 用月租找
+    public List<House> findByPricePerMonthBetween(
+            int startPricePerMonth, int endPricePerMonth);
+    // 用是不是套房找
+    public List<House> findByIsSuite(boolean isSuite);
+    // 用樓層找
+    public List<House> findByFloor(int floor);
 }
