881289518e5ca1f34a7dfed4d0a51dd0fd16cde9
Jefferson Chen <f1jefferson@yahoo.com.tw>
2023-06-20 13:05:34 +0800

    lock added

--- rental_web/src/main/java/com/group18/rental_web/repository/HouseRepo.java
+++ rental_web/src/main/java/com/group18/rental_web/repository/HouseRepo.java
@@ -2,27 +2,47 @@ package com.group18.rental_web.repository;
 
 import java.util.List;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.group18.rental_web.model.House;
 
 @Repository
 public interface HouseRepo extends JpaRepository<House, Integer> {
-    public List<House> findAll();
+        public List<House> findAll();
 
-    // 用月租找
-    public List<House> findByRentPerMonthBetween(
-            int startRentPerMonth, int endRentPerMonth);
-    // 用是不是套房找
-    public List<House> findByIsSuite(boolean isSuite);
-    // 用樓層找
-    public List<House> findByFloor(int floor);
-    // 用坪數找
-    public List<House> findBySizeBetween(int startSize, int endSize);
-    // selector
-    public List<House> findByRentPerMonthBetweenAndIsSuiteAndGenderAndFloorBetweenAndSizeBetween(
-            int startRentPerMonth, int endRentPerMonth, boolean isSuite, int gender, int startFloor, int endFloor,
-            double startSize, double endSize
-    );
+        // 用月租找
+        @Transactional
+        @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+        public List<House> findByRentPerMonthBetween(
+                        int startRentPerMonth, int endRentPerMonth);
+
+        // 用是不是套房找
+        @Transactional
+        @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+        public List<House> findByIsSuite(boolean isSuite);
+
+        // 用樓層找
+        @Transactional
+        @Lock(LockModeType.PESSIMISTIC_WRITE)
+        public List<House> findByFloor(int floor);
+
+        // 用坪數找
+        @Transactional
+        // @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+        @Lock(LockModeType.PESSIMISTIC_WRITE)
+        public List<House> findBySizeBetween(int startSize, int endSize);
+
+        // selector
+        @Transactional
+        // @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT)
+        @Lock(LockModeType.PESSIMISTIC_WRITE)
+        public List<House> findByRentPerMonthBetweenAndIsSuiteAndGenderAndFloorBetweenAndSizeBetween(
+                        int startRentPerMonth, int endRentPerMonth, boolean isSuite, int gender, int startFloor,
+                        int endFloor,
+                        double startSize, double endSize);
 }
--- rental_web/src/main/java/com/group18/rental_web/repository/UserRepo.java
+++ rental_web/src/main/java/com/group18/rental_web/repository/UserRepo.java
@@ -2,15 +2,26 @@ package com.group18.rental_web.repository;
 
 import java.util.Optional;
 
+import javax.persistence.LockModeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.group18.rental_web.model.User;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
+    @Transactional
+    @Lock(value = LockModeType.PESSIMISTIC_READ)
     Optional<User> findByName(String name);
+
+    @Transactional
+    @Lock(value = LockModeType.PESSIMISTIC_READ)
     Optional<User> findByEmail(String email);
+
     Boolean existsByName(String name);
+
     Boolean existsByEmail(String email);
 }
